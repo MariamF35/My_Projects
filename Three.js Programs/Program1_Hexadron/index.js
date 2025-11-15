f6b82862b42c1e21f0afbbd1d00aa5aa9d3cd3b8
Mariam F <133355222+MariamF35@users.noreply.github.com>
2025-11-15 20:53:52 +0400

    Comment out cube rendering code
    Commented out the cube rendering code for future reference.

--- Three.js Programs/Program1_Hexadron/index.js
+++ Three.js Programs/Program1_Hexadron/index.js
@@ -95,3 +95,39 @@ window.addEventListener('resize', () => {
 });
 
 animate(); // start animation
+
+
+/* CODE for Cube
+import * as THREE from "three";
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+new OrbitControls(camera, renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({
+  color: 0xffff00,
+  wireframe: true,
+});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x444444, 1.0);
+scene.add(hemisphereLight);
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.02;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate();
+*/
